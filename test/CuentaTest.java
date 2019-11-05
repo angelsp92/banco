748cf69cb43b3da9327a9ecde7d38af56cc573a2
usuario <usuario@pc21-082>
2019-11-05 17:56:07 +0100

    Commit paso 1. Creacion de la clase test de Cuenta y definicion de los metodos del test para el ingreso y retirada de dinero.

---
+++ test/CuentaTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author usuario
+ */
+public class CuentaTest {
+    private Cuenta c;
+    public CuentaTest() {
+    }
+    
+    @Before
+    public static void setUp() {
+        System.out.println("Set up");
+        c = new Cuenta("0001.0002.12.1234567890","Fulano de Tal");
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Test
+    public void testIngresar(){
+        System.out.println("Ingresar test");
+        c.Ingresar();
+        assertTrue(c.getSaldo()==1000.0);
+    }
+    
+    @Test
+    public void testRetirar(){
+        System.out.println("Retirar test");
+        c.Retirar(1000.0);
+        assertTrue(c.getSaldo()==1000.0);
+    }
+    
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
